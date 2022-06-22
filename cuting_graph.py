74cdf00cc2707982f10465b101d6e75b44468bde
unknown <insung.park123@gmail.com>
2022-06-22 15:12:50 +0900

    Edited title issue

--- cuting_graph.py
+++ cuting_graph.py
@@ -2,14 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import itertools
 import pickle
-
-from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
-
-from tensorflow.keras import layers
-from tensorflow import keras
-import tensorflow as tf
 import os
+
+from sklearn import preprocessing
+from tqdm import tqdm
 
 EPOCH = 200
 KERNEL_SIZE = 3
@@ -45,19 +41,18 @@ with open(DATA_PATH + 'RECORDS') as f:
 for i in range(len(record_lines)):
     record_list.append(str(record_lines[i].strip()))
         
-for j in range(len(record_list)):
+for j in (range(len(record_list))):
     temp_path = DATA_PATH + "mit" + record_list[j] + ".pkl"
     with open(temp_path, 'rb') as f:
         pickle_input = pickle.load(f)
-        for i in range(len(pickle_input[0])):
-            fig = plt.figure(figsize=[20, 20])
+        for i in tqdm(range(len(pickle_input[0]))):
+            fig = plt.figure(figsize=[10, 10])
             ax = fig.add_subplot(111)
             ax.axes.get_xaxis().set_visible(False)
             ax.axes.get_yaxis().set_visible(False)
             ax.set_frame_on(False)
 
             X.append(pickle_input[0][i])
-            plt.title(pickle_input[1][i])
             plt.plot(X[-1])
 
             check_ann = pickle_input[1][i]
@@ -97,23 +92,18 @@ for j in range(len(record_list)):
             temp_ann_list = list()
             if check_ann == "N":            # Normal
                 temp_ann_list.append(0)
-                plt.title("N")
 
             elif check_ann == "S":          # Supra-ventricular
                 temp_ann_list.append(1)
-                plt.title("S")
 
             elif check_ann == "V":          # Ventricular
                 temp_ann_list.append(2)
-                plt.title("V")
 
             elif check_ann == "F":          # False alarm
                 temp_ann_list.append(3)
-                plt.title("F")
 
             else:                           # Unclassed 
                 temp_ann_list.append(4)
-                plt.title("Q")
 
             print(check_ann + str(record_list[j]) + "__" + str(i) + str(pickle_input[1][i]) + ".png")
             y.append(temp_ann_list)
