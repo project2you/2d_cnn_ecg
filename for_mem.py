c624610008bdecbe22b0feb6df99b570b81c68d2
unknown <insung.park123@gmail.com>
2022-06-24 09:52:25 +0900

    Something really gonna wrong

---
+++ for_mem.py
@@ -0,0 +1,317 @@
+# %%
+# 그래픽 카드 둘로 쓰기 (두개 있을때, 하나만 있다면 0)
+# gpu idx 를 0 또는 1 로 설정하시오
+import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] ="{}".format(1) # gpu idx
+
+# %%
+# 경로에 폴더가 없으면 폴더 만들기
+import os
+
+def createDirectory(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print("Error: Failed to create the directory.")
+
+# %%
+import matplotlib.pyplot as plt
+from PIL import Image, ImageOps
+from tqdm import tqdm
+import numpy as np
+import itertools
+import pathlib
+import cv2
+
+from sklearn.model_selection import train_test_split
+from tensorflow.keras import layers
+from tensorflow import keras
+
+EPOCH = 100
+KERNEL_SIZE = 3
+BATCH_SIZE = 128
+IMAGE_HEIGHT = 256
+IMAGE_WIDTH = 256
+
+DATA_PATH = "./graph_data/"
+
+def list_to_list(input_list):
+    input_list_to_list = list(itertools.chain(*input_list))
+    return input_list_to_list
+
+# %% [markdown]
+# # 데이터 불러오기
+
+# %%
+data_dir = pathlib.Path(DATA_PATH)
+print(data_dir)
+
+image_count = len(list(data_dir.glob('*/*.png')))
+print(image_count)
+
+f = list(data_dir.glob('F/*'))
+n = list(data_dir.glob('N/*'))
+q = list(data_dir.glob('Q/*'))
+s = list(data_dir.glob('S/*'))
+v = list(data_dir.glob('V/*'))
+
+# %% [markdown]
+# # 데이터 split
+# ## train, test, validation data 나누기
+
+# %%
+parents_path = DATA_PATH
+child_path = os.listdir(parents_path)
+
+temp_converted_img = list()
+temp_ann_list = list()
+X = list()
+y = list()
+
+for pic_path in (child_path):
+    current_path = os.listdir(parents_path + pic_path)
+    print("[INFO] Current path : " + parents_path + pic_path)
+    for file_name in tqdm(current_path):
+        path_for_array = parents_path + pic_path + "/" + file_name
+        
+        img = cv2.imread(path_for_array)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img_resize = cv2.resize(img, dsize=(128, 128), interpolation=cv2.INTER_AREA)
+        temp_converted_img.append(img_resize / 255.0)
+        
+        check_ann = pic_path
+        
+        if check_ann == "N":            # Normal
+            temp_ann_list.append(0)
+        
+        elif check_ann == "S":          # Supra-ventricular
+            temp_ann_list.append(1)
+        
+        elif check_ann == "V":          # Ventricular
+            temp_ann_list.append(2)
+        
+        elif check_ann == "F":          # False alarm
+            temp_ann_list.append(3)
+        
+        else:                           # Unclassed 
+            temp_ann_list.append(4)
+    
+        y.append(temp_ann_list)
+
+# %%
+onehot_y = []
+for i in y:
+    if i == 0:
+        onehot_y.append([1,0,0,0,0])
+    elif i == 1:
+        onehot_y.append([0,1,0,0,0])
+    elif i == 2:
+        onehot_y.append([0,0,1,0,0])
+    elif i == 3:
+        onehot_y.append([0,0,0,1,0])
+    else:
+        onehot_y.append([0,0,0,0,1])
+
+# %%
+onehot_y = np.array(onehot_y)
+temp_converted_img = np.array(temp_converted_img)
+
+# %%
+X = temp_converted_img
+y = onehot_y
+
+# %%
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42, shuffle=True)
+X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.33, random_state=42, shuffle=True)
+
+print("[SIZE]\t\tNpX lenght : {}\n\t\tNpY length : {}".format(X_train.shape, y_train.shape))
+print("[SIZE]\t\tX_validation length : {}\n\t\ty_validation length : {}".format(X_val.shape, y_val.shape))
+print("[SIZE]\t\tX_test length : {}\n\t\ty_test length : {}".format(X_test.shape, y_test.shape))
+
+# %% [markdown]
+# # 모델 생성
+
+# %%
+input_size = (128, 128, 1)
+
+models = keras.Sequential([
+    # tf.keras.layers.experimental.preprocessing.Rescaling(1./255),
+    layers.Conv2D(64, kernel_size=(3, 3), activation='relu', input_shape=input_size),
+    layers.MaxPool2D(pool_size=(2, 2), strides=2),
+
+    layers.Conv2D(128, kernel_size=(2, 2), activation='relu'),
+    layers.MaxPool2D(pool_size=(2, 2), strides=2),
+
+    layers.Conv2D(256, kernel_size=(3, 3), activation='relu'),
+    layers.MaxPool2D(pool_size=(2, 2), strides=2),
+
+    layers.Conv2D(512, kernel_size=(2, 2), activation='relu'),
+    layers.MaxPool2D(pool_size=(2, 2), strides=2),
+
+    layers.Flatten(),
+    layers.Dropout(0.5),
+    layers.Dense(4096, activation='relu'),
+    layers.Dense(5, activation='softmax')
+])
+
+# %%
+models.summary()
+
+# %%
+models.compile(
+        optimizer='adam',
+        # loss="sparse_categorical_crossentropy",
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+)
+
+# %% [markdown]
+# ### 콜백 설정
+
+# %%
+# 콜백 설정
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+outDir = './cheakpoint/lefms_model/' # 이 경로에 best 모델이 저장된다.
+model_names = outDir + 'weights-{val_accuracy:.4f}.h5'
+
+def get_callbacks(patience = 50):
+    earlystop = EarlyStopping(monitor='val_accuracy', min_delta=0.0001, patience=patience)
+    model_checkpoint = ModelCheckpoint(model_names, monitor='val_accuracy', verbose=1, save_best_only=True, period = 1)
+  
+    # callbacks = [earlystop, model_checkpoint]     # earlystop 사용하고 싶으면 이거 풀고 아래꺼 주석 처리
+    callbacks = [model_checkpoint]
+    return callbacks
+
+# %% [markdown]
+# # 학습하기
+
+# %%
+callbacks = get_callbacks()
+
+models_hist = models.fit(
+    X_train,
+    y_train,
+    batch_size=BATCH_SIZE,
+    epochs=EPOCH,
+    validation_data=(X_val, y_val),
+    callbacks = [callbacks]
+)
+
+# %% [markdown]
+# # 결과 시각화 하기
+
+# %%
+# 학습 된 모델의 학습 과정 시각화
+import matplotlib.pyplot as plt
+def plot_model__hist(hist):
+    path = './cheakpoint/lefms/' # loss, accuracy 그래프 저장할 path
+    createDirectory(path)
+
+    # loss 추이 그래프로 그려서 저장
+    plt.figure(figsize=(6,6))
+    plt.style.use("ggplot")
+    plt.plot(hist.history['loss'], color='b', label="Training loss")
+    plt.plot(hist.history['val_loss'], color='r', label="Validation loss")
+    plt.savefig(path + 'model_loss_hist.png')
+    plt.legend()
+    plt.show()
+
+    # accuracy 추이 그래프로 그려서 저장
+    plt.figure(figsize=(6,6))
+    plt.style.use("ggplot")
+    plt.plot(hist.history['accuracy'], color='b', label="Training accuracy")
+    plt.plot(hist.history['val_accuracy'], color='r',label="Validation accuracy")
+    plt.savefig(path + 'model_loss_hist.png')
+    plt.legend(loc = "lower right")
+    plt.show()
+
+# %%
+plot_model__hist(models_hist)
+loss,acc = models.evaluate((X_val, y_val), verbose=2)
+print("multi_model의 정확도: {:5.2f}%".format(100*acc))
+print("multi_model의 Loss: {}".format(loss))
+
+# %% [markdown]
+# # 모델 불러와서 confusion matrix 그리기
+
+# %%
+# 모델 불러오기
+reconstructed_model = keras.models.load_model("./cheakpoint/lefms_model/weights-0.9924.h5")
+
+# %%
+# 예측값 얻기
+y_pred = reconstructed_model.predict(X_test, y_test)
+
+# %%
+# one hat encoding 를 하나의 변수로 바꾸기
+new_y= []
+for val in y_test:
+    max = 0
+    cnt = 0
+    for idx, num in enumerate(val):
+        if max < num:
+            max = num
+            cnt = idx + 1
+    new_y.append(cnt)
+
+# %%
+# one hat encoding 를 하나의 변수로 바꾸기
+new_y_pred = []
+for val in y_pred:
+    max = 0
+    cnt = 0
+    for idx, num in enumerate(val):
+        if max < num:
+            max = num
+            cnt = idx + 1
+    new_y_pred.append(cnt)
+
+# %%
+# 최종 정확도 산출
+score = reconstructed_model.evaluate(X_test, y_test, verbose=1)
+print('정답률 = ', score[1],'loss=', score[0])
+
+# %% [markdown]
+# ### confusion matrix 그리기
+
+# %%
+# 개수 버전
+from sklearn.metrics import classification_report, confusion_matrix, precision_score, recall_score, f1_score
+import seaborn as sns
+
+cm2 = confusion_matrix(new_y, new_y_pred)
+sns.heatmap(cm2, annot = True, fmt = 'd', cmap= 'Reds')
+plt.xlabel('predict')
+plt.ylabel('real')
+plt.xticks([0.5, 1.5, 2.5, 3.5, 4.5], ['0 = N', '1 = S', '2 = V', '3 = F', '4 = Q'])
+plt.yticks([0.5, 1.5, 2.5, 3.5, 4.5], ['0 = N', '1 = S', '2 = V', '3 = F', '4 = Q'])
+plt.show()
+
+# %%
+# percentile 버전
+total = np.sum(cm2, axis=1)
+cm2_percentile = cm2/total[:,None]
+sns.heatmap(np.round(cm2_percentile,3), annot = True, cmap= 'Reds')
+plt.xlabel('predict')
+plt.ylabel('real')
+plt.xticks([0.5, 1.5, 2.5, 3.5, 4.5], ['0 = N', '1 = S', '2 = V', '3 = F', '4 = Q'])
+plt.yticks([0.5, 1.5, 2.5, 3.5, 4.5], ['0 = N', '1 = S', '2 = V', '3 = F', '4 = Q'])
+plt.show()
+
+# %%
+from sklearn.metrics import confusion_matrix
+confusion_matrix(new_y, new_y_pred)
+
+# %%
+# classification_report 그리기
+from sklearn.metrics import classification_report
+target_names = ['0 = N', '1 = S', '2 = V', '3 = F', '4 = Q']
+print(classification_report(new_y, new_y_pred, target_names=target_names))
+
+# %%
+
+
+
